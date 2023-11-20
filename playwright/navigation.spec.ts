11969104fb5540d5a3fc98e85daa508f971884f6
Val Sab <valerie.sabando@gmail.com>
2023-11-20 14:49:56 +0100

    Adapt .env.local.example set value in ""

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,9 +1,5 @@
-import test, { expect } from '@playwright/test';
+import test from '@playwright/test';
 
 test('navigation test', async ({ page }) => {
   await page.goto('http://localhost:3000');
-
-  await expect(
-    page.getByRole('heading', { name: 'Relive your memories' }),
-  ).toBeVisible();
 });
